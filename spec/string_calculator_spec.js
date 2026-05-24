c9698c4a6f5675c9c0636b688240dc3d98e5bb09
Nadine Danner <if25b037@technikum-wien.at>
2026-05-24 10:09:15 +0200

    failingtest for testcase 3

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -8,10 +8,17 @@ describe('String Calculator', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
 
+
 	//testcase 2
 	it('should return the number itself when a single number is passed', function() {
 	expect(calculator.add('1')).toEqual(1);
-});
+	});
+
+	//testcase 3
+	it('should return sum of two comma-separated numbers', function() {
+	expect(calculator.add('1,2')).toEqual(3);
+	});
+
 
 
 });
