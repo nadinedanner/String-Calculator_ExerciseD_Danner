8ce3ce15c2515abc825faab729e65623bf07705d
Nadine Danner <if25b037@technikum-wien.at>
2026-05-24 10:03:26 +0200

    failingtest for testcase 2

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,7 +3,15 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
+	//testcase 1
 	it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
+
+	//testcase 2
+	it('should return the number itself when a single number is passed', function() {
+	expect(calculator.add('1')).toEqual(1);
+});
+
+
 });
