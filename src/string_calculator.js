ecc1251cfda191657c1c1ff9cc83135312a7ff1e
Nadine Danner <if25b037@technikum-wien.at>
2026-05-24 10:10:33 +0200

    testcase 3 done, testcase 4 in progess

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -5,5 +5,10 @@ StringCalculator.prototype.add = function(string_numbers) {
 		return 0;
 	}
 
+	if (string_numbers.includes(',')) {
+		const parts = string_numbers.split(',');
+		return parseInt(parts[0]) + parseInt(parts[1]);
+	}
+
 	return parseInt(string_numbers);
 };
